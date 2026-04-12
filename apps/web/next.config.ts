80735f439224d43858dd5a3a61f2cbcfa9a18f29
Hkhan2712 <nguynkit3107@gmail.com>
2026-04-12 21:59:46 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@kha-thien-logistics/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
